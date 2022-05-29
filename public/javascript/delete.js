9847abe26f49240ffe4563cf61d81567c2705d36
Rosemarie <rosemarie.m.halim@gmail.com>
2022-05-29 18:45:14 -0400

    used module code for javascript as reference added signup code

---
+++ public/javascript/delete.js
@@ -0,0 +1,16 @@
+
+const deletePostHandler = async function(event) {
+  console.log("clicked", event)
+  event.preventDefault();
+  const postId = document.getElementById('post-id')
+
+  fetch("/api/post/" + postId.value, {
+      method: "delete"
+  })
+  .then(function() {
+      document.location.replace("/dashboard");
+  })
+  .catch(err => console.log(err))
+}
+
+document.querySelector("#deleteBtn").addEventListener("click", deletePostHandler);
